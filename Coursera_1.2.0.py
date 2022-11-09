8a5bd90c9e5d7e5e1903604a006c1526a4c5076a
tony970404 <117787090+tony970404@users.noreply.github.com>
2022-11-08 21:52:37 -0500

    Add files via upload
    updated i/o version

--- Coursera_1.2.0.py
+++ Coursera_1.2.0.py
@@ -5,8 +5,31 @@ Spyder Editor
 This is a temporary script file.
 """
 
-def PatternCount(Pattern, Text):
-  return Text.count(Pattern)
+# def PatternCount(Pattern, Text):
+#     return Text.count(Pattern)
+# print(PatternCount('ACTAT','ACAACTATGCATACTATCGGGAACTATCCT'))
 
-print (PatternCount('ACTAT','ACAACTATGCATACTATCGGGAACTATCCT'))
+Pattern = input("please enter a pattern")
+Text = input("then, enter a part of the sequence")
+print(Text.count(Pattern))
 
+# def PatCot():
+#     p = input("Please enter a pattern")
+#     t = input("then, enter a part of the sequence")
+#     print( t.count(p))
+# PatCot()
+
+
+# def clinic():
+#     print "You've just entered the clinic!"
+#     print "Do you take the door on the left or the right?"
+#     answer = raw_input("Type left or right and hit 'Enter'.").lower()
+#     if answer == "left" or answer == "l":
+#         print "This is the Verbal Abuse Room, you heap of parrot droppings!"
+#     elif answer == "right" or answer == "r":
+#         print "Of course this is the Argument Room, I've told you that already!"
+#     else:
+#         print "You didn't pick left or right! Try again."
+#         clinic()
+
+# clinic()
