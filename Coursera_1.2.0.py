ea2ec408b9453fc5fc646cb01d000a01b78cf6c3
tony970404 <117787090+tony970404@users.noreply.github.com>
2022-11-08 20:57:32 -0500

    Add files via upload
    Cousera 1.2.0 Counting patterns

---
+++ Coursera_1.2.0.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+def PatternCount(Pattern, Text):
+  return Text.count(Pattern)
+
+print (PatternCount('ACTAT','ACAACTATGCATACTATCGGGAACTATCCT'))
+
